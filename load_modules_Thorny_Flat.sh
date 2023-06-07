46af74e6a68d9c84cd5599dc5b9a8a157b8012a7
Guillermo Avendano-Franco <gufranco@mail.wvu.edu>
2023-06-07 07:33:11 -0400

    Support for Nvidia HPC compilers on Thorny Flat

--- load_modules_Thorny_Flat.sh
+++ load_modules_Thorny_Flat.sh
@@ -11,5 +11,8 @@ then
 elif [ "$1" == "gcc122" ]
 then
     module load lang/gcc/12.2.0 libs/hdf5/1.14.0_gcc122 lang/python/cpython_3.11.3_gcc122 parallel/openmpi/4.1.5_gcc122 
+elif [ "$1" == "nvhpc" ]
+then
+    module load lang/gcc/9.3.0 lang/python/cpython_3.10.5_gcc93 libs/hdf5/1.12.2_gcc93 lang/nvidia/nvhpc
 fi
 
